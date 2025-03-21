bd5fa3f53b3cdbf885d51b53cf1a656355bf23bf
Guilherme <guilhermebarroso166@gmail.com>
2025-03-21 17:41:30 -0300

    lista de links criadas para desktops

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,31 +1,36 @@
 import styles from './Header.module.css'
 import { useState, useEffect } from 'react';
-import { FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
+import { FaShoppingCart, FaBars } from 'react-icons/fa';
 import { IoPerson,IoClose  } from "react-icons/io5";
 
 export default function Header(){
     const [menuOpen,setMenuOpen] = useState(false)
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
     useEffect(()=>{
         const handleResize = () =>{
+            setIsMobile(window.innerWidth <= 600);
             if(window.innerWidth>600){
                 setMenuOpen(false)
+                
             }
         }
         window.addEventListener('resize',handleResize)
         return () => window.removeEventListener('resize',handleResize)
     },[])
+   
     return(
         <header>
             <img src="./img/Picsart_25-03-21_14-56-48-912.png" alt="" className={styles.logo}/>
             {/*Ícone do menu hamburguer */}
-            <button className={styles.menuButton} onClick={()=> setMenuOpen(!menuOpen)}>
+            {isMobile && ( <button className={styles.menuButton} onClick={()=> setMenuOpen(!menuOpen)}>
                 {menuOpen ? '': <FaBars/>}
-            </button>
+            </button> )}
             {/*Menu lateral */}
+            {isMobile &&(
             <nav className={`${styles.sideMenu} ${menuOpen ? styles.open : ''}`}>
                 <IoClose className={styles.closeMenu} onClick={()=> setMenuOpen(!menuOpen)}/>
-                <ul className={styles.links}>
+                <ul className={styles.linksMobile}>
                     <li>Home</li>
                     <li>Fale Conosco</li>
                     <li>Tailandesas</li>
@@ -34,7 +39,16 @@ export default function Header(){
                     <li><IoPerson /> Minha Conta</li>
                 </ul>
 
-            </nav>
+            </nav>)}
+             {/* Mostra a lista normal em telas maiores que 600px */}
+             {!isMobile && (
+                <ul className={styles.links}>
+                    <li>Home</li>
+                    <li>Fale Conosco</li>
+                    <li className={styles.iconsLink}><FaShoppingCart /></li>
+                    <li className={styles.iconsLink}><IoPerson /></li>
+                </ul>
+            )}
             
         </header>
     )
